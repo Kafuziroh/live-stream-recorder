214c4673b67fe8d08cfbfe89d2317b2dfc4481a4
かふう@Kafuziroh <kafuziroh@outlook.com>
2019-07-11 19:33:14 +0800

    Fix youtube recorder

--- record_youtube.sh
+++ record_youtube.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Twitch Live Stream Recorder
+# Youtube Live Stream Recorder
 
 if [[ ! -n "$1" ]]; then
   echo "usage: $0 youtube_channel_id|live_url [format] [loop|once] [interval]"
@@ -22,43 +22,39 @@ while true; do
     LOG_PREFIX=$(date +"[%Y-%m-%d %H:%M:%S]")
     echo "$LOG_PREFIX Checking \"$LIVE_URL\"..."
 
-    # Try to get video id and title of current live stream.
-    # Add parameters about playlist to avoid downloading
-    # the full video playlist uploaded by channel accidently.
-
-    # METADATA=$(youtube-dl --get-id --get-title --get-description \
-    #   --no-playlist --playlist-items 1 \
+    # Using youtube-dl to get video id and title of current live stream
+    # Add parameters about playlist to avoid downloading the full video playlist uploaded by channel accidently
+    # METADATA=$(youtube-dl --get-id --get-title --get-description --no-playlist --playlist-items 1 \
     #   --match-filter is_live "$LIVE_URL" 2>/dev/null)
     # [[ -n "$METADATA" ]] && break
 
+	# Using wget to check the stream availability
     wget -q -O- https://www.youtube.com/channel/$1/live|grep -q '\\"isLive\\":true' && break
-
+	
+	# Retry after [interval] seconds if the stream is not available
     echo "$LOG_PREFIX The stream is not available now."
     echo "$LOG_PREFIX Retry after $INTERVAL seconds..."
     sleep $INTERVAL
   done
 
-  #Get metadata while live stream is available
-  METADATA=$(youtube-dl --get-id --get-title --get-description \
-    --no-playlist --playlist-items 1 \
-    --match-filter is_live "${LIVE_URL}" 2>/dev/null)
+  # Get metadata while live stream is available
+  METADATA=$(youtube-dl --get-id --get-title --get-description --no-playlist --playlist-items 1 "${LIVE_URL}" 2>/dev/null)
 
   # Extract video id of live stream
   ID=$(echo "$METADATA" | sed -n '2p')
 
-  # Record using MPEG-2 TS format to avoid broken file caused by interruption
+  # Record using MPEG-2 TS format to avoid broken file caused by interruption and save the metadate to file
   FNAME="youtube_${ID}_$(date +"%Y%m%d_%H%M%S").ts"
-  # Also save the metadate to file
   echo "$METADATA" > "$FNAME.info.txt"
 
   # Print logs
   echo "$LOG_PREFIX Start recording, metadata saved to \"$FNAME.info.txt\"."
   echo "$LOG_PREFIX Use command \"tail -f $FNAME.log\" to track recording progress."
 
-  # Start recording
+  # Using ffmpeg to record
   # ffmpeg -i "$M3U8_URL" -codec copy -f mpegts "$FNAME" > "$FNAME.log" 2>&1
 
-  # Use streamlink to record for HLS seeking support
+  # Using streamlink to record
   streamlink --hls-live-restart --loglevel debug -o "$FNAME" \
     "https://www.youtube.com/watch?v=${ID}" "$FORMAT" > "$FNAME.log" 2>&1
 
